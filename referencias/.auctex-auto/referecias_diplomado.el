7b304b013826f1056d14ca816d931f16ac574000
Juan Sebastian Vinasco Salinas <js.vinasco.s@gmail.com>
2025-11-11 20:20:39 -0500

    Init the repo

---
+++ referencias/.auctex-auto/referecias_diplomado.el
@@ -0,0 +1,24 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "referecias_diplomado"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Nine-advanced-training-course-on-land-remote-sensing"
+    "brosinsky2022beyond"
+    "grizonnet2017orfeo"
+    "cresson2018framework"
+    "pml1Book"
+    "pml2Book"
+    "manual2013introduction"
+    "hastie2009introduction"
+    "zheng2021two"
+    "saah2004partitioning"
+    "Miller_2024"
+    "sen2venus"
+    "szeliski2022computer"
+    "blaschke2013object"
+    "olaya2009sistemas"
+    "accuracy_9th_esa_advance_course"))
+ '(or :bibtex :latex))
+
